29582e5a034bb5b36d50b47b65ff8a38858887cb
vasylnadtochii <vasyl@stayopn.com>
2023-04-05 01:07:44 +0300

    Initial Commit

---
+++ flo/floApp.swift
@@ -0,0 +1,17 @@
+//
+//  floApp.swift
+//  flo
+//
+//  Created by Vasyl Nadtochii on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct floApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
